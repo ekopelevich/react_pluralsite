85444a45b53065146b9e9e9d868b3063bdb654f4
Elana Kopelevich <ekopelevich@gmail.com>
2016-02-04 21:55:25 -0700

    infinite comment capabilities

--- app.js
+++ app.js
@@ -1,60 +1,40 @@
 /** @jsx React.DOM */
-(function(){
-  'use strict';
 
-  var post = {
-    title: 'Look at my Cat!',
-    content: 'Really, look at my amazing cat. He is so adorable.',
-    comments: ['Your cat is cute', 'I like your cat.', 'Sweet cat, yo.']
-  };
+var post = {
+  title: 'Look at my Cat!',
+  content: 'Really, look at my amazing cat. He is so adorable.',
+  comments: ['Your cat is cute', 'I like your cat.', 'Sweet cat, yo.']
+};
 
-  var Post = React.createClass({
-    render: function(){
-      return (<div>
-          <h1>{this.props.data.title}</h1>
-          <p>{this.props.data.content}</p>
-          <h2>Comments</h2>
+var Post = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>{this.props.data.title}</h1>
+        <p>{this.props.data.content}</p>
+        <h2>Comments</h2>
 
-          <Comment content={this.props.data.comments[0]} />
-          <Comment content={this.props.data.comments[1]} />
-          <Comment content={this.props.data.comments[2]} />
+        {this.props.data.comments.map(function(comment){
+          return <Comment content={comment} />;
+        })}
+      </div>);
+  }
+});
 
-        </div>);
-    }
-  });
+var Comment = React.createClass({
+  render: function(){
+    return (<div>{this.props.content}</div>)
+  }
+});
 
-  var Comment = React.createClass({
-    render: function(){
-      return (<div>{this.props.content}</div>)
-    }
-  })
-
-  React.render(<Post data={post} />, document.body);
-
-  var Quiz = React.createClass({
-    render: function(){
-      return <div>test</div>;
-    }
-  });
+React.render(<Post data={post} />, document.getElementById('app'));
 
 
-  React.render((<Quiz data = {"foo"} />), document.getElementById('app'));
-
-  var Outer = React.createClass({
-    render: function(){
-      return <h3>This is the outer element
-              <Inner />
-              This is the rest of the otter \(typo and it stays) element
-              </h3>
-    }
-  });
-
-  var Inner = React.createClass({
-    render: function(){
-      return <p>This is an inner element</p>
-    }
-  });
-
-  React.render(<Outer />, document.getElementById('app'));
-
-})();
+  // var Quiz = React.createClass({
+  //   render: function(){
+  //     return <div>test</div>;
+  //   }
+  // });
+  //
+  //
+  // React.render((<Quiz data = {"foo"} />), document.getElementById('app'));
