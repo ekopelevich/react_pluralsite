45e9a7b3537e4430de9f263e64ee7afc61c43ada
Elana Kopelevich <ekopelevich@gmail.com>
2016-02-04 21:31:32 -0700

    this is basically facebook

--- app.js
+++ app.js
@@ -2,6 +2,35 @@
 (function(){
   'use strict';
 
+  var post = {
+    title: 'Look at my Cat!',
+    content: 'Really, look at my amazing cat. He is so adorable.',
+    comments: ['Your cat is cute', 'I like your cat.', 'Sweet cat, yo.']
+  };
+
+  var Post = React.createClass({
+    render: function(){
+      return (<div>
+          <h1>{this.props.data.title}</h1>
+          <p>{this.props.data.content}</p>
+          <h2>Comments</h2>
+
+          <Comment content={this.props.data.comments[0]} />
+          <Comment content={this.props.data.comments[1]} />
+          <Comment content={this.props.data.comments[2]} />
+
+        </div>);
+    }
+  });
+
+  var Comment = React.createClass({
+    render: function(){
+      return (<div>{this.props.content}</div>)
+    }
+  })
+
+  React.render(<Post data={post} />, document.body);
+
   var Quiz = React.createClass({
     render: function(){
       return <div>test</div>;
