9844c8157901119894293410884d8dcee5248183
Elana Kopelevich <ekopelevich@gmail.com>
2016-02-04 22:01:08 -0700

    Now I have a list of books

--- app.js
+++ app.js
@@ -1,40 +1,23 @@
 /** @jsx React.DOM */
 
-var post = {
-  title: 'Look at my Cat!',
-  content: 'Really, look at my amazing cat. He is so adorable.',
-  comments: ['Your cat is cute', 'I like your cat.', 'Sweet cat, yo.']
-};
+(function() {
+  'use strict'
 
-var Post = React.createClass({
-  render: function(){
-    return (
-      <div>
-        <h1>{this.props.data.title}</h1>
-        <p>{this.props.data.content}</p>
-        <h2>Comments</h2>
-
-        {this.props.data.comments.map(function(comment){
-          return <Comment content={comment} />;
+  var Quiz = React.createClass({
+    render: function(){
+      return <div>
+        {this.props.books.map(function(b){
+          return <Book title={b} />
         })}
-      </div>);
-  }
-});
+      </div>;
+    }
+  });
 
-var Comment = React.createClass({
-  render: function(){
-    return (<div>{this.props.content}</div>)
-  }
-});
+  var Book = React.createClass({
+    render: function() {
+      return <div><h4>{this.props.title}</h4></div>;
+    }
+  })
 
-React.render(<Post data={post} />, document.getElementById('app'));
-
-
-  // var Quiz = React.createClass({
-  //   render: function(){
-  //     return <div>test</div>;
-  //   }
-  // });
-  //
-  //
-  // React.render((<Quiz data = {"foo"} />), document.getElementById('app'));
+  React.render((<Quiz books = {['The Lord of the Rings', 'Everything is Illuminated', 'The Giver']} />), document.getElementById('app'));
+})()
