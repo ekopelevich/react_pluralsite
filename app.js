efbd96f426c16f33a62c2fe9d7c683838c4b18fb
Elana Kopelevich <ekopelevich@gmail.com>
2016-02-06 08:53:46 -0700

    propType validators

--- app.js
+++ app.js
@@ -4,7 +4,10 @@
   'use strict'
 
   var Quiz = React.createClass({
-    render: function(){
+    propTypes: {
+      books: React.PropTypes.array.isRequired
+    },
+    render: function() {
       return <div>
         {this.props.books.map(function(b){
           return <Book title={b} />
@@ -14,6 +17,9 @@
   });
 
   var Book = React.createClass({
+    propTypes: {
+      title: React.PropTypes.string.isRequired
+    },
     render: function() {
       return <div><h4>{this.props.title}</h4></div>;
     }
